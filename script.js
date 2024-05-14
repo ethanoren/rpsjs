08997bb43f23ea693af6612815b882c6b695f016
ethanoren <139882095+ethanoren@users.noreply.github.com>
2024-05-14 17:08:56 +0000

    Created function to get the choice of the human player and return it as a string

--- script.js
+++ script.js
@@ -8,13 +8,31 @@ console.log("Hello World");
 function getComputerChoice() {
     let i = Math.random()
     if (i < 0.3) {
-        return "rock"
+        return "rock";
     }
     else if (i < 0.6) {
-        return "paper"
+        return "paper";
     }
     else {
-        return "scissors"
+        return "scissors";
     }
-        
+
 }
+
+// prompt() the user to get their choice, and set it to a var playerChoice
+// if the toLowerCase() of playerChoice is "rock" "paper" or "scissors" we can just return the toLowerCase() of playerChoice
+
+function getHumanChoice() {
+    let playerChoice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
+    if (playerChoice === "rock" ||
+        playerChoice === "paper" ||
+        playerChoice === "scissors") {
+        return playerChoice;
+    }
+    else {
+        console.log("dumb stupid idiot");
+        return "rock";
+    }
+}
+
+
