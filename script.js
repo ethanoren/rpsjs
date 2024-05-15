03725eec95df0710475cb804db575a82d06c20db
ethanoren <139882095+ethanoren@users.noreply.github.com>
2024-05-15 16:17:20 +0000

    Added ability to detect if player+cpu tied (both chose same option). Also added a function that loops 5 times to play a game of 5 rounds total.

--- script.js
+++ script.js
@@ -37,23 +37,38 @@ function getHumanChoice() {
 
 var humanScore = 0, computerScore = 0;
 
-// take the human choice and computer choice
+// playround - take the human choice and computer choice
 
 // if humanchoice = rock && computerchoice = paper OR humanchoice = paper && computerchoice = scissor OR humanchoice = scissor && computerchoice = rock
 
 // same as above but for human win
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("You lose! " + computerChoice + " beats " + humanChoice);
-        computerChoice++;
+// if tie, say tie
+
+
+
+
+// playgame - loop 5 times, get computer choice and human choice each time then play a round with each
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock") {
+            console.log("You lose! " + computerChoice + " beats " + humanChoice);
+            computerChoice++;
+        }
+        else if (computerChoice === "rock" && humanChoice === "paper" || computerChoice === "paper" && humanChoice === "scissors" || computerChoice === "scissors" && humanChoice === "rock") {
+            console.log("You win! " + humanChoice + " beats " + computerChoice);
+        }
+
+        else {
+            console.log("Tie. You both chose " + humanChoice)
+        }
     }
-    else if (computerChoice === "rock" && humanChoice === "paper" || computerChoice === "paper" && humanChoice === "scissors" || computerChoice === "scissors" && humanChoice === "rock") {
-        console.log("You win! " + humanChoice + " beats " + computerChoice);
+    for (let j = 0; j <= 4; j++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection)
+playGame()
