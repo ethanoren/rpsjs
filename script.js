2be8adb3bc7c800ee612af3ded88472d27a90efb
ethanoren <139882095+ethanoren@users.noreply.github.com>
2024-05-30 17:14:56 +0000

    Added buttons that play a round based on which one clicked.

--- script.js
+++ script.js
@@ -19,56 +19,34 @@ function getComputerChoice() {
 
 }
 
-// prompt() the user to get their choice, and set it to a var playerChoice
-// if the toLowerCase() of playerChoice is "rock" "paper" or "scissors" we can just return the toLowerCase() of playerChoice
-
-function getHumanChoice() {
-    let playerChoice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
-    if (playerChoice === "rock" ||
-        playerChoice === "paper" ||
-        playerChoice === "scissors") {
-        return playerChoice;
-    }
-    else {
-        console.log("dumb stupid idiot");
-        return "rock";
-    }
-}
-
 var humanScore = 0, computerScore = 0;
-
-// playround - take the human choice and computer choice
-
-// if humanchoice = rock && computerchoice = paper OR humanchoice = paper && computerchoice = scissor OR humanchoice = scissor && computerchoice = rock
-
-// same as above but for human win
-
-// if tie, say tie
-
-
-
+var result = ""
 
 // playgame - loop 5 times, get computer choice and human choice each time then play a round with each
-function playGame() {
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock") {
-            console.log("You lose! " + computerChoice + " beats " + humanChoice);
-            computerChoice++;
-        }
-        else if (computerChoice === "rock" && humanChoice === "paper" || computerChoice === "paper" && humanChoice === "scissors" || computerChoice === "scissors" && humanChoice === "rock") {
-            console.log("You win! " + humanChoice + " beats " + computerChoice);
-        }
-
-        else {
-            console.log("Tie. You both chose " + humanChoice)
-        }
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock") {
+        updateDisplay("You lose the round! " + computerChoice + " beats " + humanChoice);
+        computerChoice++;
     }
-    for (let j = 0; j <= 4; j++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
+    else if (computerChoice === "rock" && humanChoice === "paper" || computerChoice === "paper" && humanChoice === "scissors" || computerChoice === "scissors" && humanChoice === "rock") {
+        updateDisplay("You win the round! " + humanChoice + " beats " + computerChoice);
+        humanChoice++;
+    }
+
+    else {
+        updateDisplay("Round has tied. You both chose " + humanChoice);
     }
 }
 
+//
+function updateDisplay(result) {
+    
+}
 
-playGame()
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+
+rockButton.addEventListener("click", () => playRound("rock", getComputerChoice()));
+paperButton.addEventListener("click", () => playRound("paper", getComputerChoice()));
+scissorsButton.addEventListener("click", () => playRound("scissors", getComputerChoice()));
