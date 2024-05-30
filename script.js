b7a80a051f1461f13ed5757917b4179058c99b7a
ethanoren <139882095+ethanoren@users.noreply.github.com>
2024-05-30 17:32:10 +0000

    Finished display, and added a function to update the display whenever a round is played. Also added code to stop the game from being played after the computer or player wins

--- script.js
+++ script.js
@@ -19,18 +19,21 @@ function getComputerChoice() {
 
 }
 
+var humanChoice = "";
+var computerChoice = "";
 var humanScore = 0, computerScore = 0;
-var result = ""
+var result = "";
 
 // playgame - loop 5 times, get computer choice and human choice each time then play a round with each
 function playRound(humanChoice, computerChoice) {
+    if (humanScore >= 5 || computerScore >= 5) {return}
     if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
         updateDisplay("You lose the round! " + computerChoice + " beats " + humanChoice);
-        computerChoice++;
     }
     else if (computerChoice === "rock" && humanChoice === "paper" || computerChoice === "paper" && humanChoice === "scissors" || computerChoice === "scissors" && humanChoice === "rock") {
+        humanScore++;
         updateDisplay("You win the round! " + humanChoice + " beats " + computerChoice);
-        humanChoice++;
     }
 
     else {
@@ -38,15 +41,31 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-//
+// take result, and set the text content of the result div to the player score, computer score, and the result of the round. if any score >= 5, the result will instead say who won the game
 function updateDisplay(result) {
-    
+    if (humanScore >= 5) { 
+        result = "You won the game!";
+    }
+    else if (computerScore >= 5) {
+        result = "You lost the game!"
+    }
+    scoreDisplay.textContent = "Player score: " + humanScore + "\nComputer score: " + computerScore + "\n" + result;
 }
 
+const scoreDisplay = document.getElementById("scoreDisplay")
 const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
 
-rockButton.addEventListener("click", () => playRound("rock", getComputerChoice()));
-paperButton.addEventListener("click", () => playRound("paper", getComputerChoice()));
-scissorsButton.addEventListener("click", () => playRound("scissors", getComputerChoice()));
+rockButton.addEventListener("click", () => {
+    humanChoice = "rock"
+    playRound("rock", getComputerChoice())
+});
+paperButton.addEventListener("click", () => {
+    humanChoice = "paper"
+    playRound("paper", getComputerChoice())
+});
+scissorsButton.addEventListener("click", () => {
+    humanChoice = "scissors"
+    playRound("scissors", getComputerChoice())
+});
